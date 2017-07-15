a1f050fc16c06f7e8ff9d4dc9209a6f42623b112
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2017-07-15 02:13:46 -0500

    Added new manual tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,99 @@ module.exports = function(grunt) {
 				cwd:  'tests',
 				src:  ['**/?.js', '!out/**'],
 				dest: 'tests/out'
+			},
+			amd: {
+				options: {
+					root: 'uhop.test',
+					browserGlobals: {
+						'boom':    'BOOM',
+						'./d':     '!uhop.D',
+						'./sub/f': '!uhop.F'
+					},
+					replacements: {
+						'c.js': 'shim.js'
+					},
+					newLoader: '--amd'
+				},
+				cwd:  'tests',
+				src:  ['**/?.js', '!out/**'],
+				dest: 'tests/out'
+			},
+			cjs: {
+				options: {
+					root: 'uhop.test',
+					browserGlobals: {
+						'boom':    'BOOM',
+						'./d':     '!uhop.D',
+						'./sub/f': '!uhop.F'
+					},
+					replacements: {
+						'c.js': 'shim.js'
+					},
+					newLoader: '--cjs'
+				},
+				cwd:  'tests',
+				src:  ['**/?.js', '!out/**'],
+				dest: 'tests/out'
+			},
+			es6: {
+				options: {
+					root: 'uhop.test',
+					browserGlobals: {
+						'boom':    'BOOM',
+						'./d':     '!uhop.D',
+						'./sub/f': '!uhop.F'
+					},
+					replacements: {
+						'c.js': 'shim.js'
+					},
+					newLoader: '--es6'
+				},
+				cwd:  'tests',
+				src:  ['**/?.js', '!out/**'],
+				dest: 'tests/out'
+			},
+			static: {
+				options: {
+					root: 'uhop.test',
+					browserGlobals: {
+						'boom':    'BOOM',
+						'./d':     '!uhop.D',
+						'./sub/f': '!uhop.F'
+					},
+					replacements: {
+						'c.js': 'shim.js'
+					},
+					newLoader: 'static'
+				},
+				cwd:  'tests',
+				src:  ['**/?.js', '!out/**'],
+				dest: 'tests/out'
+			},
+			dynamic: {
+				options: {
+					root: 'uhop.test',
+					browserGlobals: {
+						'boom':    'BOOM',
+						'./d':     '!uhop.D',
+						'./sub/f': '!uhop.F'
+					},
+					replacements: {
+						'c.js': 'shim.js'
+					},
+					newLoader: function (deps, mod, from) {
+						return '/* ' + JSON.stringify({deps: deps, mod: mod, from: from}) + ' */';
+					}
+				},
+				cwd:  'tests',
+				src:  ['**/?.js', '!out/**'],
+				dest: 'tests/out'
 			}
 		}
 	});
 
 	grunt.loadTasks('tasks');
 
-	grunt.registerTask('default', 'transform_amd');
-	grunt.registerTask('test',    'transform_amd');
+	grunt.registerTask('default', 'transform_amd:batch');
+	grunt.registerTask('test',    'transform_amd:batch');
 };
