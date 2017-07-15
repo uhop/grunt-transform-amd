3f540d821c2e36203458fa88a14f7683117dd9a3
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2017-07-15 02:13:21 -0500

    Added new prologue options.

--- tasks/transform_amd.js
+++ tasks/transform_amd.js
@@ -3,8 +3,11 @@
 
 var path = require('path');
 
+var Globals = require('heya-globalize/Globals');
+var prologueGlobals = require('heya-globalize/prologueGlobals');
+var prologueRequire = require('heya-globalize/prologueRequire');
+var prologueImport  = require('heya-globalize/prologueImport');
 var processFile = require('heya-globalize/processFile');
-var Globals     = require('heya-globalize/Globals');
 
 
 var defaultLoaders = ['/* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})'];
@@ -27,9 +30,37 @@ module.exports = function (grunt) {
 				return;
 			}
 
-			var globals = new Globals(options.browserGlobals, options.root),
-				loaders = options.loaders || defaultLoaders,
-				pf = processFile(globals, loaders, options.newLoader);
+			var loaders = options.loaders || defaultLoaders, newLoader;
+
+			if (typeof options.newLoader == 'string') {
+				switch (options.newLoader) {
+					case '--amd':
+						!options.silent && console.log('converting to AMD...');
+						newLoader = 'define';
+						break;
+					case '--cjs':
+						!options.silent && console.log('converting to CommonJS...');
+						newLoader = prologueRequire;
+						break;
+					case '--es6':
+						!options.silent && console.log('converting to ES6 modules...');
+						newLoader = prologueImport;
+						break;
+					default:
+						!options.silent && console.log('using static prologue...');
+						newLoader = options.newLoader;
+						break;
+				}
+			} else if (typeof options.newLoader == 'function') {
+				!options.silent && console.log('using dynamic prologue...');
+				newLoader = options.newLoader;
+			}
+			if (!newLoader) {
+				!options.silent && console.log('converting to browser globals...');
+				newLoader = prologueGlobals(new Globals(options.browserGlobals, options.root));
+			}
+
+			var pf = processFile(loaders, newLoader);
 
 			this.files.forEach(function (file) {
 				file.src.forEach(function (name) {
